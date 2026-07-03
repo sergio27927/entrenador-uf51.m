1b31e866b4387c7e24c993b25225c94ca8e82df1
sergio27927 <sergio_27_9@hotmail.es>
2026-07-03 18:06:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const boton = document.getElementById("empezar");
+const pregunta = document.getElementById("pregunta");
 
+boton.addEventListener("click", () => {
+    pregunta.innerHTML = `
+        <h2>Pregunta 1</h2>
+        <p>¿Qué es el Derecho Penal?</p>
+
+        <button>A) El conjunto de normas que regulan los delitos y las penas.</button><br><br>
+        <button>B) Un reglamento deportivo.</button><br><br>
+        <button>C) Una ley de tráfico.</button>
+    `;
+});
